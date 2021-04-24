3c97e786cbc37ef742c9dc24cddc6665fee7873a
arijit <arijitdeb1@gmail.com>
2021-04-24 21:11:25 +0530

    final commit

--- src/main/java/com/arijit/git/service/GitTestService.java
+++ src/main/java/com/arijit/git/service/GitTestService.java
@@ -7,5 +7,17 @@ public class GitTestService {
 
     public void FirstService(String name){
         System.out.println("commit 1 for reset check by master 1");
+        System.out.println("commit 3 for reset check by master 1");
+    }
+
+    public void SecondService(String name){
+        System.out.println("commit 1 for reset check by master 1");
+        System.out.println("commit 2 for reset check by master 1");
+        System.out.println("commit 3 for reset check by master 1");
+        System.out.println("commit 4 for reset check by master 1");
+        System.out.println("commit 5 for reset check by master 1");
+        System.out.println("commit 6 for reset check by master 1");
+
+
     }
 }
