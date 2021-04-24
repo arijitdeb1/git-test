4dc7610511b2262a2fd299ef3c1fca288ef77796
arijit <arijitdeb1@gmail.com>
2021-04-24 20:17:41 +0530

    commit 8 master 2

--- src/main/java/com/arijit/git/controller/GitTestController.java
+++ src/main/java/com/arijit/git/controller/GitTestController.java
@@ -10,7 +10,6 @@ public class GitTestController {
     @GetMapping("/hello?/{name}")
     public String sayHello(@PathVariable String name){
         System.out.println("Commit from master 2 and from master 1");
-        System.out.println("Commit from master 1 ate rebase");
         System.out.println("another commit from master 2 after rebase 2");
         return "Hello "+name;
     }
