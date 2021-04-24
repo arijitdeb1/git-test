5fa07de07057806e17c3509a06ea933cdf0bb88b
arijit <arijitdeb1@gmail.com>
2021-04-24 20:42:11 +0530

    commit 11 master 1

---
+++ src/main/java/com/arijit/git/service/GitTestService.java
@@ -0,0 +1,11 @@
+package com.arijit.git.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GitTestService {
+
+    public void FirstService(String name){
+        System.out.println("commit 1 for reset check by master 1");
+    }
+}
